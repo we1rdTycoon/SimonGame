47972d5df6c2f8fbb3d139cbb03a7feacc3fe87a
My Name <myEmail@example.com>
2020-08-22 23:54:31 +0700

    ddd

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 
 
 <template>
-<div>
+<div class="ff">
+ <div>
     <div class="qwe">
-      <div class="upleft block"  v-on:click="leftop"></div><div class="upright block"  v-on:click="rigtop"></div>
-       <b-button class="qq" v-on:click="start" variant="danger">START</b-button>
+      <div class="upleft block"  v-on:click="leftop"></div><div class="upright block"  v-on:click="rigtop"></div>  
       </div>
       <div class="qwe">
         <div class="downleft block"  v-on:click="lefdown"></div><div class="downright block"  v-on:click="rigdown"></div>
       </div>
-       <p v-if="err">Первый параграф</p>     
+      
+  </div>
+  <div>
+    <p>Round {{n}}</p> 
+    <b-button class="qq" v-on:click="start" variant="danger">START</b-button>
+     <p class="error" v-if="err">ПРОВАЛ</p>  
+  </div>
 </div>
 </template>
 
@@ -26,57 +32,77 @@ import { map, concatMap, delay } from 'rxjs/operators';
   export default {
    data: function(){
         return {
-            sequance: [0,1,3,2,2,1,0,3,2,3],
+            sequance: [1,2],
             mysequance:[],
             err:false,
+            ready:false,
+            n:1,
         }
         
     },
     methods:{
        start: function(){
-         this.err=false;
-          let a = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
-          this.sequance.push(0);
+          this.err=false;
+          this.ready=false;
+          this.add();
+          this.n=1;
          
        },
        compare:function(){
           for (let i = 0; i < this.mysequance.length; i++) { // выведет 0, затем 1, затем 2
-           console.log("ffff");
               if(this.mysequance[i]!=this.sequance[i]){
                 this.err=true;
                 break;
               }
               if(i==this.sequance.length-1){
-                
+                 this.n++;
+                 this.ready=false;
+                 this.add();
               }
           }
        },
+       add: function(){
+          let a = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
+          this.sequance.push(a);
+          this.mysequance=[];
+          console.log(this.sequance);
+          console.log(this.mysequance);
+
+       },
 
        leftop: function(){
+         if(this.ready==true){
           this.mysequance.push(0);
           console.log(this.mysequance)
           this.compare();
+         }
        },
        rigtop: function(){
+         if(this.ready==true){
           this.mysequance.push(1);
           console.log(this.mysequance)
           this.compare();
+         }
        },
        lefdown: function(){
+         if(this.ready==true){
           this.mysequance.push(2);
           console.log(this.mysequance)
           this.compare();
+         }
        },
        rigdown: function(){
+         if(this.ready==true){
           this.mysequance.push(3);
           console.log(this.mysequance)
           this.compare();
+         }
        },
     },
     watch: {
             sequance: function (newNumber) {
               from(this.sequance).pipe(concatMap(item => of(item).pipe(delay(1000))))
-              .subscribe(function (next){
+              .subscribe( (next)=>{
                    switch (next) {
                        case 0:
                          $(".upleft").fadeTo("normal",1,function(){
@@ -100,6 +126,8 @@ import { map, concatMap, delay } from 'rxjs/operators';
                        break;
  
                      }
+              },null,()=>{
+                this.ready=true;
               }
                     
                 );
@@ -135,14 +163,33 @@ div.block{
   height: 300px;
   vertical-align: top;
   opacity: 0.5;
+  cursor: pointer;
 }
+
+div.block:hover{
+  border: 4px double yellow; /* Параметры границы */
+}
+
 div.qwe{
   display: flex;
 }
 
 .qq{
   align-self: center;
-  
+  width: 320px;
+}
+.ff{
+  display: flex;
+  flex-wrap: wrap-reverse;
+  justify-content: space-around;
+  align-items: center;
+}
+
+p{
+  font-size: 65pt;
+}
+p.error{
+  color: red;
 }
 
 
