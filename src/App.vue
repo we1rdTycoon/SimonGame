8f82c42a54c2c769fbb7a76736745bfa7ed81d9d
My Name <myEmail@example.com>
2020-08-24 21:57:27 +0700

    fix bags

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 
 <template>
 <div class="ff">
- 
  <div>
     <div class="qwe">
       <div class="upleft block"  v-on:click="leftop"></div><div class="upright block"  v-on:click="rigtop"></div>  
@@ -18,7 +17,6 @@
      <p class="error" v-if="err">ПРОВАЛ</p> <br><br>
         <b-form-radio class="edd" v-model="mode" name="some-radios" value="SimpleGame">SIMPLE GAME</b-form-radio>
         <b-form-radio class="edd" v-model="mode" name="some-radios" value="SoundOnly">SOUND ONLY</b-form-radio>
-      
   </div>
 </div>
 </template>
@@ -26,18 +24,16 @@
 <script>
 import $ from "jquery";
 import { of } from 'rxjs';
-import { skip } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { from } from 'rxjs';
 import { interval } from 'rxjs';
 import { ignoreElements } from 'rxjs/operators';
-import { debounceTime } from 'rxjs/operators';
 import { map, concatMap, delay } from 'rxjs/operators';
 
   export default {
    data: function(){
         return {
-            sequance: [],
+            sequance: [], 
             mysequance:[],
             err:false,
             ready:false,
@@ -49,33 +45,40 @@ import { map, concatMap, delay } from 'rxjs/operators';
     },
     methods:{
        start: function(){
-          this.sequance=[],
-          this.err=false;
-          this.ready=false;
-          this.add();
-          this.n=1;
-          this.stop=false
+         this.stop=true;
+         interval(700).pipe(take(1)).subscribe((x)=>{
+              if(x==0){
+                 this.sequance=[];
+                 this.err=false;
+                 this.ready=false;
+                 this.n=1;
+                 this.stop=false
+                this.add();
+              }
+
+         });
+         
+          
          
        },
        compare:function(){
-          for (let i = 0; i < this.mysequance.length; i++) { // выведет 0, затем 1, затем 2
+          for (let i = 0; i < this.mysequance.length; i++) {
               if(this.mysequance[i]!=this.sequance[i]){
                 this.err=true;
                 this.ready=false;
                 break;
               }
               if(i==this.sequance.length-1){
-                const numbers = interval(700);
-                const takeFourNumbers = numbers.pipe(take(2));
-                takeFourNumbers.subscribe((x)=>{ 
+                 interval(700).pipe(take(1)).subscribe((x)=>{ 
                    console.log('Next: ', x)
-                   if(x==1){
+                   if(x==0){
+                     if(this.stop==false){
                      var audio = new Audio('dist/win.mp3');
                      audio.play();
-                     console.log(x);
                      this.n++;
                      this.ready=false;
                      this.add();
+                     }
                    }
                 });
                
@@ -86,9 +89,6 @@ import { map, concatMap, delay } from 'rxjs/operators';
           let a = Math.floor(Math.random() * (3 - 0 + 1)) + 0;
           this.sequance.push(a);
           this.mysequance=[];
-          console.log(this.sequance);
-          console.log(this.mysequance);
-
        },
 
        leftop: function(el){
@@ -96,11 +96,9 @@ import { map, concatMap, delay } from 'rxjs/operators';
             var audio = new Audio('dist/sound1.mp3');
              $(".upleft").fadeTo("fast",1,function(){
                  $(".upleft").fadeTo("fast",0.5);
-                 
              });
             audio.play();
             this.mysequance.push(0);
-            console.log(el)
             this.compare();
          }
        },
@@ -114,7 +112,6 @@ import { map, concatMap, delay } from 'rxjs/operators';
              
           audio.play();
           this.mysequance.push(1);
-          console.log(this.mysequance)
           this.compare();
          }
        },
@@ -126,7 +123,6 @@ import { map, concatMap, delay } from 'rxjs/operators';
                });
             audio.play();
           this.mysequance.push(2);
-          console.log(this.mysequance)
           this.compare();
          }
        },
@@ -138,14 +134,22 @@ import { map, concatMap, delay } from 'rxjs/operators';
                });
             audio.play();
           this.mysequance.push(3);
-          console.log(this.mysequance)
           this.compare();
          }
        },
     },
     watch: {
             sequance: function (newNumber) {
-              from(this.sequance).pipe(concatMap((item) =>{if(this.stop==false) { return of(item).pipe(delay(1000))} else { return of(item).pipe(ignoreElements())} }))
+              if(this.sequance.length!==0){
+              from(this.sequance).pipe(concatMap((item) =>{
+                if(this.stop==false) {
+                   return of(item).pipe(delay(1000))
+                   }
+                    else 
+                    {
+                       return of(item).pipe(ignoreElements())
+                    }
+                 }))
               .subscribe( (next)=>{
                    switch (next) {
                        case 0:
@@ -199,83 +203,21 @@ import { map, concatMap, delay } from 'rxjs/operators';
               }
                     
                 );
-                 
+              }
             },
-            mode:function(dsad){
+            mode:function(){
                this.mysequance=[];
                this.sequance=[];
                this.err=false;
                this.ready=false;
                this.n=1;
                this.stop=true;
+               this.ready=false;
             }
         },
   }
 </script>
 
 <style>
-body{
- background: -webkit-linear-gradient(45deg, #EECFBA, #C5DDE8);
-}
-
-div.upleft{
-  background: black; /* Цвет фона */
-  border-top-left-radius: 300px;
-  margin-left: 10px;
-}
-div.upright{
-  background: blue; /* Цвет фона */
-   border-top-right-radius: 300px;
-}
-div.downleft{
-  background: red; /* Цвет фона */
-  border-bottom-left-radius: 300px;
-  margin-left: 10px;
-}
-div.downright{
-   border-bottom-right-radius: 300px;
-   background: #007E3E; /* Цвет фона */
-}
-
-div.block{
-  color: #fff; /* Цвет текста */
-  width: 300px;
-  height: 300px;
-  vertical-align: top;
-  opacity: 0.5;
-  cursor: pointer;
-}
-
-div.block:hover{
-  border: 4px double yellow; /* Параметры границы */
-}
-
-div.qwe{
-  display: flex;
-}
-
-.qq{
-  align-self: center;
-  width: 320px;
-}
-.ff{
-  display: flex;
-  flex-wrap: wrap-reverse;
-  justify-content: space-around;
-  align-items: center;
-  margin-top: 20px;
-  overflow: hidden;
-}
-
-p{
-  font-size: 65pt;
-}
-p.error{
-  color: red;
-}
-
-.edd{
-  font-family: 'McLaren';
-  font-size: 20px;
-}
+@import "style.css";
 </style>
